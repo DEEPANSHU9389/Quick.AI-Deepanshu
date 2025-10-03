f5c678b0825f018b9f1993dbcd56143045775633
deepanshu <deepanshukumar2022@gmail.com>
2025-10-03 09:48:40 +0530

    Your commit message

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 
 const Hero = () => {
 
-    const navigate = useNavigate 
+    const navigate = useNavigate() 
 
 
 
@@ -12,7 +12,7 @@ const Hero = () => {
     <div className='px-4 sm:px-20 xl:px-32 relative inline-flex flex-col w-full justify-center bg-[url(/gradientBackground.png)] bg-no-repeat min-h-screen'>
 
         <div className='text-center mb-6'>
-            <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create amazing content <br /> with <span className='text-blue-600'>Ai tools</span></h1>
+            <h1 className='text-3xl sm:text-5xl md:text-6xl 2xl:text-7xl font-semibold mx-auto leading-[1.2]'>Create amazing content <br /> with <span className='text-blue-600'>AI tools</span></h1>
             <p className='mt-4 max-w-xs sm:max-w-lg 2xl:max-w-xl m-auto max-sm:text-xs text-gray-600'>Transform your content creation with our suite of premium Ai tools, write articles, generate images, and enhance your workflow.</p>
         </div>
       
